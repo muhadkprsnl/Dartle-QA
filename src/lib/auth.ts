7811707dd2a495cbac678fa4dba454e6f5fbd268
muhadk <muhadk123@gmail.com>
2025-07-05 09:24:43 +0530

    environment and login api updated

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,7 +19,8 @@
 
 export const authService = {
     login: async (username: string, password: string) => {
-        const res = await fetch('http://localhost:3001/api/login', {
+        // const res = await fetch('http://localhost:3001/api/login',
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
